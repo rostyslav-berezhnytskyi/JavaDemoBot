69164cc1be50f490836edb36fe0ea57edea8e8c0
Rostyslav <rostik2510@gmail.com>
2025-01-22 10:37:00 +0200

    Add dependencie telegrambots to pom.xml

---
+++ src/main/java/org/example/javademobot/JavaDemoBotApplication.java
@@ -0,0 +1,13 @@
+package org.example.javademobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDemoBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDemoBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/javademobot/JavaDemoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javademobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDemoBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

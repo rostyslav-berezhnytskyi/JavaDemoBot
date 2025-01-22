87da0e52b9678ae5f4c1aeb7067a45455d47451e
rostyslav-berezhnytskyi <rostik2510@gmail.com>
2025-01-22 18:33:21 +0200

    Add Lombok dependencie, add package config with class BotConfig

---
+++ src/main/java/org/example/javademobot/config/BotConfig.java
@@ -0,0 +1,14 @@
+package org.example.javademobot.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("application.properties")
+public class BotConfig {
+    @Value("bot.name")
+    String botName;
+    @Value("bot.token")
+    String token;
+}

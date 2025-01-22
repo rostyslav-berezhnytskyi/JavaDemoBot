f7d7e98606c1e35d08b6aa964ed118c6c89ca5d9
rostyslav-berezhnytskyi <rostik2510@gmail.com>
2025-01-22 23:09:50 +0200

    Add new classes TelegramBot, BotInitializer, wrote realisation for this classes and ad basic functionalyti for telegramBot like /start, /getCurrentTime etc.

--- src/main/java/org/example/javademobot/config/BotConfig.java
+++ src/main/java/org/example/javademobot/config/BotConfig.java
@@ -1,14 +1,15 @@
 package org.example.javademobot.config;
 
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("application.properties")
+@Data
 public class BotConfig {
-    @Value("bot.name")
+    @Value("${bot.name}")
     String botName;
-    @Value("bot.token")
+    @Value("${bot.token}")
     String token;
 }
---
+++ src/main/java/org/example/javademobot/service/TelegramBot.java
@@ -0,0 +1,66 @@
+package org.example.javademobot.service;
+
+import org.example.javademobot.config.BotConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.time.LocalDateTime;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+    final BotConfig config;
+
+    @Autowired
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if(update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            switch (messageText) {
+                case "/start" -> startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                case "/getCurrentTime" -> sendMessage(chatId, "The current time of your request is " + LocalDateTime.now());
+                case "/jaba" -> sendMessage(chatId, "kurka");
+                case "/kurka" -> sendMessage(chatId, "jaba");
+                default -> sendMessage(chatId, "Sorry, command was not recognize");
+            }
+        }
+    }
+
+    private void startCommandReceived(long chatId, String name) {
+        String answer = "Hi, " + name + ", nice to meet you!";
+
+        sendMessage(chatId, answer);
+    }
+
+
+    private void sendMessage(long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+
+        try{
+            execute(message);
+        } catch (TelegramApiException e) {
+            System.out.println(e + "exeption");
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getToken();
+    }
+}

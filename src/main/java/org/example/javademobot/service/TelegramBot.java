b152fb74fafd75c82892b978136b79ee6139fcfc
rostyslav-berezhnytskyi <rostik2510@gmail.com>
2025-01-31 17:31:14 +0200

    Create menu for bot, add new command in menu /help /mydata, change name of command /getCurrentTime to /get_time

--- src/main/java/org/example/javademobot/service/TelegramBot.java
+++ src/main/java/org/example/javademobot/service/TelegramBot.java
@@ -5,20 +5,35 @@ import org.example.javademobot.config.BotConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
     final BotConfig config;
+    static final String HELP_TEXT = """
+            This bot is created to demonstrate basic Java Spring Boot Telegram Bot possibilities
+            You can execute commands from the main menu on the left or by taping a command:
+            
+            Type /start to see a welcome message
+            
+            Type /mydata to see data stored about yourself
+            
+            Type /help to see this message again""";
 
     @Autowired
     public TelegramBot(BotConfig config) {
         this.config = config;
+        createListOfCommands();
     }
 
     @Override
@@ -29,7 +44,8 @@ public class TelegramBot extends TelegramLongPollingBot {
 
             switch (messageText) {
                 case "/start" -> startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
-                case "/getCurrentTime" -> sendMessage(chatId, "The current time of your request is " + LocalDateTime.now());
+                case "/get_time" -> sendMessage(chatId, "The current time of your request is " + LocalDateTime.now());
+                case "/help" -> sendMessage(chatId, HELP_TEXT);
                 case "/jaba" -> sendMessage(chatId, "kurka");
                 case "/kurka" -> sendMessage(chatId, "jaba");
                 default -> sendMessage(chatId, "Sorry, command was not recognize");
@@ -50,9 +66,26 @@ public class TelegramBot extends TelegramLongPollingBot {
         message.setText(textToSend);
 
         try{
-            execute(message);
+            this.execute(message);
         } catch (TelegramApiException e) {
             log.error("Error occurred: " + e.getMessage());
+        }
+    }
+
+    private void createListOfCommands() {
+        List<BotCommand> listOfCommands = new ArrayList<>();
+        listOfCommands.add(new BotCommand("/start", "get a welcom message"));
+        listOfCommands.add(new BotCommand("/mydata", "get your data stored"));
+        listOfCommands.add(new BotCommand("/deletedata", "delete my data stored"));
+        listOfCommands.add(new BotCommand("/help", "info how to use bot"));
+        listOfCommands.add(new BotCommand("/settings", "set your preferences"));
+        listOfCommands.add(new BotCommand("/get_time", "return current date and time"));
+        listOfCommands.add(new BotCommand("/jaba", "return kurka"));
+        listOfCommands.add(new BotCommand("/kurka", "return jaba"));
+        try {
+            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            log.error("Error getting bot`s command lis: " + e.getMessage());
         }
     }
 
